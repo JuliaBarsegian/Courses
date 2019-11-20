0be6800a6d962a9ef9290215f9fafcef7f890c83
= <=>
2019-11-20 22:00:43 +0300

    Updated task1

---
+++ src/HW_08_11_19/Task1/Interval.java
@@ -0,0 +1,5 @@
+package HW_08_11_19.Task1;
+
+public interface Interval {
+    void showTime(int time);
+}
--- src/HW_08_11_19/Task1/Timer.java
+++ src/HW_08_11_19/Task1/Timer.java
@@ -1,15 +1,8 @@
 package HW_08_11_19.Task1;
 
-public class Timer {
-    public static void main(String[] args) {
-        System.out.println("Подождите 10 секунд");
-        try {
-            while (true) {
-                Thread.sleep(10 * 1000);
-                System.out.println("Прошло 10 секунд");
-            }
-        } catch (Exception e) {
-            System.out.println("Получили исключение");
-        }
+public class Timer implements Interval {
+    @Override
+    public void showTime(int time) {
+        System.out.println("Прошло " + time + " секунд");
     }
 }
